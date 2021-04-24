46ff709b54133ff95f2f97cb0b5b8201e9d7de3d
J Ben Anderson <revovisions@gmail.com>
2021-04-24 15:15:05 -0400

    added todo input and submit logic

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, { text: value, isCompleted: false }];
+    setTodos(newTodos);
+    setValue("");
+  };
   return (
     <>
       {todos.map((todo, i) => (
@@ -20,6 +30,15 @@ function App() {
           {todo.text}
         </div>
       ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add Todo..."
+          onChange={(e) => setValue(e.target.value)}
+        ></input>
+      </form>
     </>
   );
 }
